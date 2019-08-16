7b1ad208aa4e18e20204370a33ff3023fc9cda29
long <37194356+longyang85GitHub@users.noreply.github.com>
2019-08-16 16:04:44 -0400

    t3

--- application.py
+++ application.py
@@ -16,6 +16,96 @@ from string import punctuation
 from heapq import nlargest
 # Build a List of Stopwords
 stopwords = list(STOP_WORDS)
+nlp = spacy.load('en')
+def text_summarizer(raw_docx):
+    raw_text = raw_docx
+    docx = nlp(raw_text)
+    stopwords = list(STOP_WORDS)
+    # Build Word Frequency
+  #  word.text is tokenization in spacy
+    word_frequencies = {}  
+    for word in docx:  
+        if word.text not in stopwords:
+            if word.text not in word_frequencies.keys():
+                word_frequencies[word.text] = 1
+            else:
+                word_frequencies[word.text] += 1
+    maximum_frequncy = max(word_frequencies.values())
+    for word in word_frequencies.keys():  
+        word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+    # Sentence Tokens
+    sentence_list = [ sentence for sentence in docx.sents ]
+
+    # Calculate Sentence Score and Ranking
+    sentence_scores = {}  
+    for sent in sentence_list:  
+        for word in sent:
+            if word.text.lower() in word_frequencies.keys():
+                if len(sent.text.split(' ')) < 30:
+                    if sent not in sentence_scores.keys():
+                        sentence_scores[sent] = word_frequencies[word.text.lower()]
+                    else:
+                        sentence_scores[sent] += word_frequencies[word.text.lower()]
+
+    # Find N Largest
+    summary_sentences = nlargest(6, sentence_scores, key=sentence_scores.get)
+    final_sentences = [ w.text for w in summary_sentences ]
+    summary = ' '.join(final_sentences)
+    #print("Original Document\n")
+    #print(raw_docx)
+    #print("Total Length:",len(raw_docx))
+    #print('\n\nSummarized Document\n')
+    #print(summary)
+    #print("Total Length:",len(summary))
+    return summary
+def create_text_analytics_table(text_string):
+    text = []
+    ORG =[]
+    Date =[]
+    EVENT=[]
+    Money=[]
+    GPE=[]
+    doc = nlp(text_string)
+    for ent in doc.ents:
+        if ent.label_=="ORG":
+            text.append(ent.text)
+            ORG.append(ent.label_)
+            Date.append("")
+            EVENT.append("")
+            Money.append("")
+            GPE.append("")
+        elif ent.label_=="DATE":
+            text.append(ent.text)
+            Date.append(ent.label_)
+            ORG.append("")
+            EVENT.append("")
+            Money.append("")
+            GPE.append("")
+        elif ent.label_=="EVENT":
+            text.append(ent.text)
+            EVENT.append(ent.label_)
+            Date.append("")
+            ORG.append("")
+            Money.append("")
+            GPE.append("")
+        elif ent.label_=="MONEY":
+            text.append(ent.text)
+            Money.append(ent.label_)
+            Date.append("")
+            ORG.append("")
+            EVENT.append("")
+            GPE.append("")
+        elif ent.label_=="GPE":
+            text.append(ent.text)
+            Money.append("")
+            Date.append("")
+            ORG.append("")
+            EVENT.append("")
+            GPE.append(ent.label_)
+    narr = np.array([text,ORG,Date,EVENT,Money,GPE],dtype = object)
+    narr_t = np.transpose(narr)
+    df = pd.DataFrame(narr_t,columns=['Text','lable:ORG','lable:Date','lable:Event','lable:Money','lable:GPE'])
+    return df
 
 ##
 now = datetime.datetime.now()
@@ -140,13 +230,17 @@ JPY_6m_JPY_Libor_result.loc['Average'] = JPY_6m_JPY_Libor_result.mean()
 
 ##=======================================================
 app = Flask(__name__)
+class ReusableForm(Form):
+    Document = TextField('Document:',validators=[validators.required()])
 #index/home
 @app.route('/')
 def index():
     return render_template('home.html',csv=US_3m_Libot_result,csv1=US_6m_Libot_result)
+
 @app.route('/IBRD_Benchmarks', methods=("POST", "GET"))
 def IBRD_Benchmarks():
     return render_template('IBRD_Benchmarks.html',csv=US_3m_Libot_result,total_rows=len(US_3m_Libot_result.axes[0]),total_cols=len(US_3m_Libot_result.axes[1]))
+
 @app.route('/nlp', methods=("POST", "GET"))
 def nlp():
     return render_template('nlp.html',csv=US_3m_Libot_result,total_rows=len(US_3m_Libot_result.axes[0]),total_cols=len(US_3m_Libot_result.axes[1]))
