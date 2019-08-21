1ef7bd0fc231536bb5416d1c762b26e9839d6e94
long <37194356+longyang85GitHub@users.noreply.github.com>
2019-08-21 16:37:00 -0400

    t1

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 import nltk
-from nltk.corpus import wordnet   #Import wordnet from the NLTK
-from nltk.corpus import wordnet as wn
+#from nltk.corpus import wordnet   #Import wordnet from the NLTK
+from nltk.corpus.reader import wordnet as wn
 import os
 import numpy as np
 import pandas as pd
