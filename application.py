b059c125b5015bf50c3f7fed43455544c65888a7
long <37194356+longyang85GitHub@users.noreply.github.com>
2019-09-06 09:42:47 -0400

    change wordnet

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 import nltk
+#nltk.download('wordnet')
 from nltk.corpus import wordnet   #Import wordnet from the NLTK
 import os
 import numpy as np
@@ -21,7 +22,6 @@ from spacy.matcher import Matcher
 nlp = en_core_web_sm.load()
 # Build a List of Stopwords
 stopwords = list(STOP_WORDS)
-
 
 def text_summarizer(raw_docx):
     raw_text = raw_docx
